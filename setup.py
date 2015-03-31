9bd20b48d35b3f8c47b01a19efbebb0e3263e6e3
Joseph Anderson <janderson@agero.com>
2015-03-31 08:44:33 -0500

    changed license to Apache

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.AirbiquityDevice"
 VERSION = "1.1.1"
 AUTHOR = "Joseph Anderson"
-LICENSE = "GPLv2+"
+LICENSE = "Apache"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.AirbiquityDevice']
 INSTALL_REQUIRES = ['ZenPacks.community.ConstructionKit']
